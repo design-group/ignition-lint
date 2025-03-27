f6b79a0252da3cc0b490705920a6cd3759ce5e95
Alex Spyksma <alex.spyksma@bwdesigngroup.com>
2025-03-27 13:28:37 -0700

    add init.py

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[],  # Add dependencies here
+    install_requires=[],  # Add dependencies if needed
     entry_points={
         "console_scripts": [
             "ignition-lint = ignition_lint:main",
         ],
     },
+    author="Eric Knorr",
+    description="A linter for Ignition JSON files",
+    license="MIT",
+    url="https://github.com/design-group/ignition-lint",
 )
