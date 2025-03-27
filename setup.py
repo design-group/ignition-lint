3b5f2552ef8e96a96732c2e4268ea24b8f475b35
Alex Spyksma <alex.spyksma@bwdesigngroup.com>
2025-03-27 16:02:08 -0700

    fixed referecne import

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
+import os
+
+print("Current directory:", os.getcwd())
+print("Contents of src:", os.listdir("src"))
+packages = find_packages(where="src")
+print("Found packages:", packages)
 
 setup(
     name="ignition-lint",
     version="0.1.0",
-    packages=find_packages(where="src"),  # Automatically find packages in src/
-    package_dir={"": "src"},  # Map the root package to src/
+    packages=packages,
+    package_dir={"": "src"},
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "ignition-lint = ignition_lint.ignition_lint:main",  # Updated to package.module:function
+            "ignition-lint = ignition_lint:main",
         ],
     },
     author="Eric Knorr",
